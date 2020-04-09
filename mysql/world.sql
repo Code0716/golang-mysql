ed650330b101a3d0cbc4deb91198a6a1953c834d
code0716 <job_info@code0716.com>
2020-04-09 22:53:51 +0900

    fix sql

--- mysql/world.sql
+++ mysql/world.sql
@@ -36,12 +36,12 @@ DROP TABLE IF EXISTS `city`;
 CREATE TABLE `city` (
   `ID` int NOT NULL AUTO_INCREMENT,
   `Name` char(35) NOT NULL DEFAULT '',
-  `CountryCode` char(3) NOT NULL DEFAULT '',
+  `Code` char(3) NOT NULL DEFAULT '',
   `District` char(20) NOT NULL DEFAULT '',
   `Population` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`ID`),
-  KEY `CountryCode` (`CountryCode`),
-  CONSTRAINT `city_ibfk_1` FOREIGN KEY (`CountryCode`) REFERENCES `country` (`Code`)
+  KEY `Code` (`Code`),
+  CONSTRAINT `city_ibfk_1` FOREIGN KEY (`Code`) REFERENCES `country` (`Code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
