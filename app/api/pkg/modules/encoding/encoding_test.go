efbf9d44125aa9dac49e52ccc90c79256bd74f87
code0716 <job_info@code0716.com>
2021-05-25 18:10:23 +0900

    add test

--- app/api/pkg/modules/encoding/encoding_test.go
+++ app/api/pkg/modules/encoding/encoding_test.go
@@ -1,12 +1,27 @@
 package encoding
 
-import "testing"
+import (
+	"encoding/base64"
+	"os"
+	"testing"
+)
 
 func TestEncodeBase64(t *testing.T) {
+	savePath := "/images/upload/"
+	fileName := "0BCAE30B-2BE8-4CF4-82AB-1B49455F88BA.jpg"
+	file, err := os.Open(savePath + fileName)
+	if err != nil {
+		t.Error("ファイルが読み込めませんでした。")
+	}
 	type args struct {
 		savePath string
 		fileName string
 	}
+	fi, _ := file.Stat() // interface
+	size := fi.Size()    // file size
+
+	data := make([]byte, size)
+	file.Read(data)
 	tests := []struct {
 		name string
 		args args
@@ -15,10 +30,10 @@ func TestEncodeBase64(t *testing.T) {
 		{
 			name: "test",
 			args: args{
-				savePath: "../../images/upload/",
-				fileName: "BADCCFAE-143D-40A4-8D34-93E6D0729660-1.jpp",
+				savePath: savePath,
+				fileName: fileName,
 			},
-			want: "../../images/upload/BADCCFAE-143D-40A4-8D34-93E6D0729660-1.jpg",
+			want: base64.StdEncoding.EncodeToString(data),
 		},
 	}
 	for _, tt := range tests {
@@ -28,4 +43,5 @@ func TestEncodeBase64(t *testing.T) {
 			}
 		})
 	}
+
 }
